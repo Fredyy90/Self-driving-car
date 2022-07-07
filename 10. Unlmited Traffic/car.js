2e2277e0f90d956b9230da987997624db10f4605
Fredyy <frederick.behrends@gmail.com>
2022-07-07 14:24:22 +0200

    2nd hidden layer and cars know their speed and angle

--- 10. Unlmited Traffic/car.js
+++ 10. Unlmited Traffic/car.js
@@ -21,7 +21,7 @@ class Car{
         if(controlType!="DUMMY"){
             this.sensor=new Sensor(this);
             this.brain=new NeuralNetwork(
-                [this.sensor.rayCount,8,4]
+                [this.sensor.rayCount+2,6,6,4]
             );
         }
         this.controls=new Controls(controlType);
@@ -93,7 +93,8 @@ class Car{
             const offsets=this.sensor.readings.map(
                 s=>s==null?0:1-s.offset
             );
-            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+            const inputs = [this.angle/Math.PI*2, this.speed/this.maxSpeed, ...offsets];
+            const outputs=NeuralNetwork.feedForward(inputs,this.brain);
 
             if(this.useBrain){
                 this.controls.forward=outputs[0];
